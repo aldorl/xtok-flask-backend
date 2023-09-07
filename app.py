fd6a2078eda490dd48b8020da0a380a3a0009cbc
Aldo Ruiz Luna <aldo.ruiz.luna@gmail.com>
2023-09-06 18:38:32 -0600

    Add test 'final image' and improves app.py file

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, send_file
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def home():
     return "Hello, World!"
+
+@app.route("/python")
+def hello_world():
+    return "<p>Hello, Python!</p>"
+
+@app.route('/generate_video', methods=['POST'])
+def generate_video():
+    # NOTE: currently only sending image as a test, eventually video will be sent
+    return send_file('python_module/generated/final_image.png', mimetype='image/png')
 
 if __name__ == '__main__':
     app.run(debug=True)
