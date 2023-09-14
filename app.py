8967ffa3208ec7beb2714fd303746dfaa5107c88
Aldo Ruiz Luna <aldo.ruiz.luna@gmail.com>
2023-09-14 13:23:08 -0600

    Updates generate_video function to process video

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, send_file
 from flask_cors import CORS
+import subprocess
 
 app = Flask(__name__)
 CORS(app)
@@ -14,8 +15,22 @@ def hello_world():
 
 @app.route('/generate_video', methods=['POST'])
 def generate_video():
-    # NOTE: currently only sending image as a test, eventually video will be sent
-    return send_file('python_module/generated/final_image.png', mimetype='image/png')
+    # NOTE: following line only sending image as a test, eventually video will be sent
+    # return send_file('python_module/generated/final_image.png', mimetype='image/png')
+    try:
+        print("Trying python subprocess", flush=True)
+        subprocess.call("python3 python_module/main.py", shell=True)
+    except Exception as e:
+        print("Exception caught", flush=True)
+        print(str(e), flush=True)
+
+    try:
+        print("About to send file...", flush=True)
+        return send_file('python_module/generated/output_video.mp4', mimetype='video/mp4')
+    except Exception as e:
+        print("Exception caught", flush=True)
+        print(str(e), flush=True)
+        return str(e), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
